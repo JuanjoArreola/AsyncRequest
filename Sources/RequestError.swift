86fde67750a956377a806373ea20833d8b0dcb61
Juanjo <juanjo.arreola@cinemasapp.co>
2018-09-27 09:48:42 -0500

    TimeoutRequest

--- Sources/RequestError.swift
+++ Sources/RequestError.swift
@@ -1,5 +1,5 @@
 import Foundation
 
 public enum RequestError: Error {
-    case canceled
+    case canceled, timeout
 }
---
+++ Sources/TimeoutRequest.swift
@@ -0,0 +1,26 @@
+//
+//  TimeoutRequest.swift
+//  AsyncRequest
+//
+//  Created by Juan Jose Arreola Simon on 9/27/18.
+//
+
+import Foundation
+
+@available(OSX 10.12, watchOS 3.0, tvOS 10.0, iOS 10.0, *)
+class TimeoutRequest<T>: Request<T> {
+    
+    private var timer: Timer?
+    
+    public init(timeout: TimeInterval, successHandler: ((T) -> Void)? = nil) {
+        super.init(successHandler: successHandler)
+        setupTimer(withTimeInterval: timeout)
+    }
+    
+    func setupTimer(withTimeInterval interval: TimeInterval) {
+        timer = Timer.scheduledTimer(withTimeInterval: interval, repeats: false, block: { [weak self] _ in
+            guard let completed = self?.completed, !completed else { return }
+            self?.complete(with: RequestError.timeout)
+        })
+    }
+}
