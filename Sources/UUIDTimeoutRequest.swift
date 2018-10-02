265cce690645adfcebc09e033bfe362ebec2fc69
Juanjo <juanjo.arreola@gmail.com>
2018-10-02 17:53:22 -0500

    UUIDTimeoutRequest

---
+++ Sources/UUIDTimeoutRequest.swift
@@ -0,0 +1,27 @@
+//
+//  UUIDTimeoutRequest.swift
+//  AsyncRequest
+//
+//  Created by Juan Jose Arreola on 10/2/18.
+//
+
+import Foundation
+
+@available(OSX 10.12, watchOS 3.0, tvOS 10.0, iOS 10.0, *)
+public class UUIDTimeoutRequest<T>: TimeoutRequest<T>, Hashable, Equatable {
+    
+    let uuid: UUID
+    
+    override init(timeout: TimeInterval, successHandler: ((T) -> Void)?) {
+        uuid = UUID()
+        super.init(timeout: timeout, successHandler: successHandler)
+    }
+    
+    public static func == (lhs: UUIDTimeoutRequest<T>, rhs: UUIDTimeoutRequest<T>) -> Bool {
+        return lhs.uuid == rhs.uuid
+    }
+    
+    public var hashValue: Int {
+        return uuid.hashValue
+    }
+}
